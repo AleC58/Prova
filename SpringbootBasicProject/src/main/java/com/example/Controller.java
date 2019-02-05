8b4d4e79b78b9c8784e9f49d0a22f94585534542
Alessandro Cazziolato <a.cazziolato@gmail.com>
2019-02-05 10:57:05 +0100

    Altre modifiche al demo

--- SpringbootBasicProject/src/main/java/com/example/Controller.java
+++ SpringbootBasicProject/src/main/java/com/example/Controller.java
@@ -10,27 +10,26 @@ import org.springframework.web.bind.annotation.RestController;
  * @author alex.cazziolato
  */
 @RestController
-public class BlogController {
+public class Controller {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Congratulations from BlogController.java!!!!";
+		return "Congratulations from BlogController.java! 333";
 	}
 
 	@RequestMapping("/ciao")
 	public String ciao() {
-		return "ciao";
-	}
-
-	//@RequestMapping("/ciaojson", produces = {"application/JSON"})
-	@RequestMapping(value = "/ciaojson", produces = {"application/JSON"})
-	@ResponseBody
-	public String getProduces() {
-		return "ciao!!!";
+		return "ciao a tutti";
 	}
 
 	@RequestMapping(value = "/ciao/{id}")
 	public String ciao(@PathVariable("id") String id) {
 		return "ciao with id=" + id;
+	}
+
+	@RequestMapping(value = "/ciaojson", produces = {"application/JSON"})
+	@ResponseBody
+	public String getCiaoJson() {
+		return "{\"eta\": 32, \"citta\": \"Livorno\", \"sesso\": \"M\"}";
 	}
 }
