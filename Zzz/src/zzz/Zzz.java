f1ea2ab64f26e0220f7600647d4a57ab03075c36
Alessandro Cazziolato <a.cazziolato@gmail.com>
2019-02-23 11:01:42 +0100

    Prova

---
+++ Zzz/src/zzz/Zzz.java
@@ -0,0 +1,16 @@
+package zzz;
+
+/**
+ *
+ * @author alex.cazziolato
+ */
+public class Zzz {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+	}
+	
+}
