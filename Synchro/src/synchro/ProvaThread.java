7a0b33d99b1f4406071ea8c811f61cfc6ef93137
Alessandro Cazziolato <a.cazziolato@gmail.com>
2019-04-01 08:20:14 +0200

    Prove Ajax

---
+++ Synchro/src/synchro/ProvaThread.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package synchro;
+import static synchro.Synchro.*;
+/**
+ *
+ * @author alex.cazziolato
+ */
+public class ProvaThread implements Runnable {
+	@Override
+	public void run() {
+		p.m();
+	}
+	
+	
+}
