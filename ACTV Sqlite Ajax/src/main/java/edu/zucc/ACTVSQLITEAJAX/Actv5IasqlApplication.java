fdac1857f639403be235afd64cdce5b0a83773d2
Alessandro Cazziolato <a.cazziolato@gmail.com>
2019-04-02 08:22:39 +0200

    ACTV Sqlite Ajax

---
+++ ACTV Sqlite Ajax/src/main/java/edu/zucc/ACTVSQLITEAJAX/Actv5IasqlApplication.java
@@ -0,0 +1,13 @@
+package edu.zucc.ACTVSQLITEAJAX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Actv5IasqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Actv5IasqlApplication.class, args);
+	}
+
+}
