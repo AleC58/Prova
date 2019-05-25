9c2028575c4cee8f4cee800c0bbc819a83e487aa
Alessandro Cazziolato <a.cazziolato@gmail.com>
2019-05-25 12:03:00 +0200

    Inizio ProvaVaadin

---
+++ provavaadin/src/main/java/edu/alec/provavaadin/ProvavaadinApplication.java
@@ -0,0 +1,24 @@
+/*
+- generare il progetto da https://start.spring.io/
+  Maven / Java / Spring boot 2.1.5
+  Nomi: edu.alec / ProvaVaadin
+  Dep.: Vaadin
+- importare il progetto (File / Import project / From Zip)
+- NB: il progetto ha errori, e non si vede "Source packages"
+      -> per risolvere:
+  Clean and build project
+*/
+
+package edu.alec.provavaadin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvavaadinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvavaadinApplication.class, args);
+	}
+
+}
